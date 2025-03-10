c99befb3909d86d80501cdb684a95887c01737d9
Rhuan Kowic Santos <dev.rhuankowic@gmail.com>
2025-03-10 08:05:46 -0300

    Finalizando desafio

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+const adicionarAmigo = () => {
+  const nome = document.getElementById("amigo").value;
+  if (nome != "") {
+    amigos.push(nome);
+  } else {
+    alert("Por favor, insira um nome.");
+  }
+  document.getElementById("amigo").value = "";
+  atualizarLista();
+};
+
+const atualizarLista = () => {
+  lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  for (i in amigos) {
+    lista.innerHTML += `<li>${amigos[i]}</li>`;
+  }
+};
+
+const sortearAmigo = () => {
+  document.getElementById("resultado").innerHTML = "";
+
+  if (amigos.length > 0) {
+    const numero = Math.floor(Math.random() * amigos.length);
+    document.getElementById(
+      "resultado"
+    ).innerHTML = `O amigo secreto é: ${amigos[numero]}`;
+  } else {
+    alert("Não há amigos para sortear. Adicione alguns!");
+  }
+};
